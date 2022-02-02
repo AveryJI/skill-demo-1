c35c35bf9439db90d6b754c1cab01f6c4b0473a0
AveryJI <yanji3927@gmail.com>
2022-02-02 14:58:55 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,7 +4,7 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.addition(1,-1));
+        assertEquals(0, SkillDemo.addition(1,-1));
     }
 
 }
